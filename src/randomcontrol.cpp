e8d5a25bc313346e399ce979844932582babb5ee
Chris Collander <cmcollander@gmail.com>
2017-07-27 11:35:21 -0500

    Added comments to written programs

--- src/randomcontrol.cpp
+++ src/randomcontrol.cpp
@@ -1,22 +1,22 @@
-#include "ros/ros.h"
-#include "ugv/motorvels.h"
+#include "ros/ros.h" // For ROS control
+#include "ugv/motorvels.h" // The description of the ugv/motorvels message type
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-	ros::init(argc, argv, "randomcontrol");
-	ros::NodeHandle n;
-	ros::Publisher pub = n.advertise<ugv::motorvels>("motors",1000);
-	ros::Rate loop_rate(1);
+	ros::init(argc, argv, "randomcontrol"); // Initialize our application 'randomcontrol' (same as file name)
+	ros::NodeHandle n; // Create a new ROS node for this application
+	ros::Publisher pub = n.advertise<ugv::motorvels>("motors",1000);  // Create a new publisher to the /motors topic, sending ugv/motorvels messages. 1000 messages can be queued at a time
+	ros::Rate loop_rate(1); // How many Hz does this signal publish at? for this, 1 cycle per second.
 
-	while(ros::ok()) {
-		ugv::motorvels msg;
+	while(ros::ok()) { // Loop as long as ROS is running, or broken out of otherwise
+		ugv::motorvels msg; // Blank ugv/motorvels message
 		
-		msg.left = rand()%2001 - 1000;
-		msg.right = rand()%2001 - 1000;
+		msg.left = rand()%2001 - 1000; // Set the left motor value between -1000 and 1000
+		msg.right = rand()%2001 - 1000; // Set the right motor value between -1000 and 1000
 		
-		pub.publish(msg);
-		ros::spinOnce();
-		loop_rate.sleep();
+		pub.publish(msg); // Publish our message
+		ros::spinOnce(); // Handle ROS
+		loop_rate.sleep(); // Pause to allow the above loop_rate
 	}
 	return 0;
 }
--- src/ugv.cpp
+++ src/ugv.cpp
@@ -10,11 +10,12 @@
 
 RoboteqDevice device;
 
+// Callback function for the /motors topic (accepts a ugv/motorvels message)
 void motorCallback(const ugv::motorvels& msg)
 {
-  device.SetCommand(_GO, 2, (int)msg.left);
+  device.SetCommand(_GO, 2, (int)msg.left); // Set our left motor
   sleepms(10); // Allow proper time between commands
-  device.SetCommand(_GO, 1, (int)msg.right);
+  device.SetCommand(_GO, 1, (int)msg.right); // Set our right motor
   sleepms(10); // Allow proper time between commands
 }
 
@@ -28,15 +29,15 @@ int main(int argc, char **argv)
     status = device.Connect("/dev/ttyACM0");
   }
 
+  // Prepare ROS
+  ros::init(argc, argv, "ugv"); // Start a new ROS application called ugv
 
-  ros::init(argc, argv, "ugv");
+  ros::NodeHandle n; // Create a new node
 
-  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("motors", 1000, motorCallback); // Create a subscriber to the /motors topic, that has a 1000-count message queue, and upon receiving a message calls the above callback function
 
-  ros::Subscriber sub = n.subscribe("motors", 1000, motorCallback);
+  ros::spin(); // continously loop ROS (obtaining and processing messages in the process)
 
-  ros::spin();
-
-  device.Disconnect();
+  device.Disconnect(); // Once broken from ROS, disconnect our motor driver.
   return 0;
 }
