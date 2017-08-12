e6cf6f29675a823fbd16e28313548d596cf6e672
Chris Collander <cmcollander@gmail.com>
2017-08-11 22:51:21 -0500

    ros_control

--- src/UGV.cpp
+++ src/UGV.cpp
@@ -12,24 +12,39 @@ private:
 	double eff[2];
 public:
 	UGV() {
+		// Initialize all values to zero
+		cmd[0] = 0; cmd[1] = 0;
+		pos[0] = 0; pos[1] = 0;
+		vel[0] = 0; vel[0] = 0;
+		eff[0] = 0; eff[0] = 0;
+
+		// Create our joints and register them
 		hardware_interface::JointStateHandle stateA("Left",&pos[0],&vel[0],&eff[0]);
-		hardware_interface::JointStateHandle stateB("Left",&pos[1],&vel[1],&eff[1]);
+		hardware_interface::JointStateHandle stateB("Right",&pos[1],&vel[1],&eff[1]);
 		jnt_state.registerHandle(stateA);
 		jnt_state.registerHandle(stateB);
-		reisterInterface(&jnt_state);
-
-		hardware_interface::JointHandle velA(jnt_state.getHandle("Right"),&cmd[0]);
+		registerInterface(&jnt_state);
+		hardware_interface::JointHandle velA(jnt_state.getHandle("Left"),&cmd[0]);
 		hardware_interface::JointHandle velB(jnt_state.getHandle("Right"),&cmd[1]);
 		jnt_vel.registerHandle(velA);
 		jnt_vel.registerHandle(velB);
 		registerInterface(&jnt_vel);
 	}
 
+	// Functions to assist with ROS time-management
+	ros::Time getTime() const {return ros::Time::now();}
+    ros::Duration getPeriod() const {return ros::Duration(0.01);}
+
 	void read() {
 		// TODO: Determine pos, vel, and eff and place them into variables
+
 	}
 
 	void write() {
 		// TODO: Write 'cmd' out to the motor driver
+		pos[0] += vel[0]*getPeriod().toSec();
+		pos[1] += vel[1]*getPeriod().toSec();
+		vel[0] = cmd[0];
+		vel[1] = cmd[1];
 	}
-}
+};
---
+++ src/UGVinterface.cpp
@@ -0,0 +1,31 @@
+// Partially based off example code at 
+// https://github.com/ros-controls/ros_controllers/blob/kinetic-devel/diff_drive_controller/test/diffbot.cpp
+
+#include "UGV.cpp"
+#include <ros/ros.h>
+#include <controller_manager/controller_manager.h>
+
+
+int main(int argc, char** argv) {
+        ros::init(argc, argv, "UGV");
+        ros::NodeHandle nh;
+
+        UGV ugv;
+        ROS_WARN_STREAM("period: " << ugv.getPeriod().toSec());
+        controller_manager::ControllerManager cm(&ugv, nh);
+
+        ros::Rate rate(1.0 / ugv.getPeriod().toSec());
+        ros::AsyncSpinner spinner(1);
+        spinner.start();
+
+        while(ros::ok()) {
+                ugv.read();
+                cm.update(ugv.getTime(), ugv.getPeriod());
+                ugv.write();
+                rate.sleep();
+        }
+        spinner.stop();
+
+        return 0;
+}
+
