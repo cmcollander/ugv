565408be4378bfcb4a8b3bfa839e776d1f2328ed
Chris Collander <cmcollander@gmail.com>
2018-12-04 16:20:55 -0600

    Replace constants and Add Documentation
    Replaced many in-code constants to allow easy adjustments for odometry and velocity calculations to the motor controller. Also included documentation on how to implement this node

--- src/UGV.cpp
+++ src/UGV.cpp
@@ -1,3 +1,28 @@
+/*
+Chris Collander, cmcollander@gmail.com, December 2018
+
+This ROS node is used for communication between /cmd_vel and /odom to a Roboteq motor controller.
+
+Please ensure your controller has the following settings:
+* Left wheel connected to motor 1
+* Right wheel connected to motor 2
+* Encoders connected, tested, and set to tuned PID coefficients in closed-loop speed control
+* Encoder ticks per revolution properly calculated through gear ratio and encoder specs
+* Max velocity for each wheel set to 60 RPM
+* Some parameters may need to be tuned and changed based on your build. Trial and error.
+
+After wheels respond to correct direction, speed can be adjusted through proper calculation (or trial and error)
+After wheels respond to correct speed, odom can be adjusted through proper calculation (or trial and error)
+
+My current method for tuning odom:
+* Visualize in Rviz with /odom as global frame.
+* View lidar and transform tree
+* Rotate robot clockwise.
+* * If lidar scan rotates to the right: Decrease ODOM_NUM and/or Increase ODOM_DEN
+* * If lidar scan rotates to the left:  Increase ODOM_NUM and/or Decrease ODOM_DEN
+* When robot is able to rotate N-rotations without rotation of the laser scan, odom is properly tuned (my N=100)
+*/
+
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/robot_hw.h>
@@ -6,6 +31,20 @@
 #include "ugv/ErrorCodes.h"
 #include "ugv/Constants.h"
 #include <unistd.h>
+
+#define PI 3.14159265359
+
+// These two values represent the numerator and denominator of the scalar values for Velocity input
+#define VEL_IN_NUM PI
+#define VEL_IN_DEN 3.0
+
+// These two values represent the numerator and denominator of the scalar values for Velocity output
+#define VEL_OUT_NUM 15.9155*0.33333333
+#define VEL_OUT_DEN 1.0
+
+// These two values represent the numerator and denominator of the scalar value from Odom.
+#define ODOM_NUM PI
+#define ODOM_DEN 6000*4.20
 
 class UGV : public hardware_interface::RobotHW {
 private:
@@ -26,6 +65,7 @@ public:
 		eff[0] = 0; eff[1] = 0;
 
 		// Create our joints and register them
+		// TODO: Why did I set these to these indices???? They work in my case though...
 		hardware_interface::JointStateHandle stateA("Right",&pos[1],&vel[0],&eff[0]);
 		hardware_interface::JointStateHandle stateB("Left",&pos[0],&vel[1],&eff[1]);
 		jnt_state.registerHandle(stateA);
@@ -50,23 +90,17 @@ public:
 
 	void read() {
 		// Determine pos, vel, and eff and place them into variables
+		
 		// _ABSPEED returns RPM, we need Rads/Sec
-
-		// vel[0] = device.GetValue(_ABSPEED,1,ret)/9.5493;
-		// vel[1] = device.GetValue(_ABSPEED,2,ret)/9.5493;
-		// pos[0] = 3.14159265359*device.GetValue(_ABCNTR,1,ret)/(6000.0);
-		// pos[1] = 3.14159265359*device.GetValue(_ABCNTR,2,ret)/(6000.0);
 		vel[0] = device.GetValue(_ABSPEED,1,ret);
-		vel[0] = 3.14159265359*ret/3.0;
+		vel[0] = ret*VEL_IN_NUM/VEL_IN_DEN;
 		vel[1] = device.GetValue(_ABSPEED,2,ret);
-		vel[1] = 3.14159265359*ret/3.0;
+		vel[1] = ret*VEL_IN_NUM/VEL_IN_DEN;
 		
 		pos[0] = device.GetValue(_ABCNTR,1,ret);
-		pos[0] = 3.14159265359*(ret-initial_encoder[0])/(6000.0);
-		pos[0] = pos[0]/4.20;
+		pos[0] = ret-initial_encoder[1]*ODOM_NUM/ODOM_DEN;
 		pos[1] = device.GetValue(_ABCNTR,2,ret);
-		pos[1] = 3.14159265359*(ret-initial_encoder[1])/(6000.0);
-		pos[1] = pos[1]/4.20;
+		pos[1] = ret-initial_encoder[1]*ODOM_NUM/ODOM_DEN;
 		
 		eff[0] = device.GetValue(_MOTPWR,1,ret);
 		eff[0] = ret;
@@ -78,9 +112,7 @@ public:
 		// Write 'cmd' out to the motor driver
 		// cmd[] is in rads per second
 		// _GO takes in a value from -1000 to 1000 linearly from -60RPM to 60RPM
-		//  cmd[x]*159.155*2
-		device.SetCommand(_GO,1,(int)(cmd[0]*15.9155*0.33333333));
-
-		device.SetCommand(_GO,2,(int)(cmd[1]*15.9155*0.33333333));
+		device.SetCommand(_GO,1,(int)(cmd[0]*VEL_OUT_NUM/VEL_OUT_DEN));
+		device.SetCommand(_GO,2,(int)(cmd[1]*VEL_OUT_NUM/VEL_OUT_DEN));
 	}
 };
